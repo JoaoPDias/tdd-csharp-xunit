c4d3748ec645667629d19d8cb7f1c2238dfdbd75
João Paulo <jpdias1997@hotmail.com>
2020-03-01 21:21:31 -0300

    Adicionar arquivos de projeto.

---
+++ test/CursoOnlineTest/Cursos/Curso.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Dynamic;
+
+namespace CursoOnline.DominioTest.Cursos
+{
+    public class Curso
+    {
+
+        public Curso(string nome, int cargaHoraria, PublicoAlvo publicoAlvo, double valor)
+        {
+            if (String.IsNullOrEmpty(nome))
+                throw new ArgumentException("O nome não pode ser vazio ou nulo");
+            if(cargaHoraria<1)
+                throw new ArgumentException("A carga horária não pode ser menor que 1");
+            if(valor< 1)
+                throw new ArgumentException("O valor não pode ser menor que 1");
+            Nome = nome;
+            CargaHoraria = cargaHoraria;
+            PublicoAlvo = publicoAlvo;
+            Valor = valor;
+        }
+
+        public string Nome { get; private set; }
+        public int? CargaHoraria { get; private set; }
+        public PublicoAlvo PublicoAlvo { get; private set; }
+        public double Valor { get; private set; }
+    }
+}
---
+++ test/CursoOnlineTest/_Util/AssertExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualBasic;
+using Xunit;
+
+namespace CursoOnline.DominioTest._Util
+{
+    public static class AssertExtensions
+    {
+        public static void WithMessage(this ArgumentException exception, string mensagem)
+        {
+            if(exception.Message.Equals(mensagem))
+                Assert.True(true);
+            else
+                Assert.False(true);
+                
+            
+        }
+
+    }
+}
